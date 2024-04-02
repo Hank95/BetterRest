0f5b12112b58ea2a33deeb0da017a59192a8da06
Henry Pendleton <hhpendleton@gmail.com>
2024-04-02 13:47:53 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Henry Pendleton on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
